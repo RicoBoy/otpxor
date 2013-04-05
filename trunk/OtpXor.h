01ac69389013b201fa766593acaa589117b54198
crashenator@gmail.com <crashenator@gmail.com@373c9fe9-2f9e-a9fb-4a9b-1a588d520857>
2013-04-05 04:45:44 +0000

    Added Extract+AutoCorrect mode that attempts to fix Offset issues

---
+++ trunk/OtpXor.h
@@ -0,0 +1,61 @@
+
+#include <cstdio>
+#include <climits>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+#define WINSIZE 256
+#define TOLSIZE 180
+#define TOLRATIO 1//0.703125
+//#define GZIP_TEST 1
+//#define MINIMAL 1
+
+
+#define Q(x) #x
+#define QQ(x) Q(x)
+#define OPER(a,b) a^b
+#define QOPER QQ(OPER(a,b))
+
+
+#pragma warning( disable : 4996 ) // disable fopen_s() warning.
+#ifdef MINIMAL
+stringstream nullcout;
+#define CONSOLE nullcout
+#else
+#define CONSOLE cout
+#endif
+#ifdef GZIP_TEST
+#define MINWINDOWCHK && j>9
+#else
+#define MINWINDOWCHK 
+#endif
+
+
+bool isWindowGzip(char* win, size_t msgsize,size_t iBin);
+string getWindowAt(char* msg, char* bin, size_t msgsize, size_t i, size_t length=INT_MAX);
+string safeWindow(char* msg, size_t msgsize);
+#define stoi(s) atoi((s).c_str())
+bool fexists(const string& filename);
+char* fload(const string& filename, size_t& filesize);
+
+void extract(char* bin, char* msg, char* out, size_t msgsize, int off);
+void extract_autocorrect_A(char* bin, char* msg, char* out, size_t msgsize, int off);
+void extract_autocorrect_B(char* bin, char* msg, char* out, size_t msgsize, int off);
+
+#define SWAPT(t,x,y) {t swptmp=x;x=y;y=swptmp;}
+
+
+
+
+enum PROG_ACT{
+	ACT_NONE,
+	ACT_HELP,
+	ACT_SCAN,
+	ACT_EXTR,
+	ACT_EXAC,
+};
+
